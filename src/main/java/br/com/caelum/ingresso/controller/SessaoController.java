1804f280b54cd80a847c27b405e828aadd90273a
Leandro <Leandro@gmail.com>
2019-10-05 16:35:09 +0000

    Criacao da entidade Sessao e criacao do controller SessaoController

---
+++ src/main/java/br/com/caelum/ingresso/controller/SessaoController.java
@@ -0,0 +1,28 @@
+package br.com.caelum.ingresso.controller;
+
+import br.com.caelum.ingresso.dao.*;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.servlet.ModelAndView;
+
+@Controller
+public class SessaoController {
+	
+	@Autowired
+	private SalaDao salaDao;
+	@Autowired
+	private FilmeDao filmeDao;
+	
+	public ModelAndView form(@RequestParam("salaId") Integer salaId) {
+		
+		ModelAndView modelAndView = new ModelAndView("sessao/sessao");
+		
+		modelAndView.addObject("sala", salaDao.findOne(salaId));
+		modelAndView.addObject("filmes", filmeDao.findAll());
+		
+		return modelAndView;
+	}
+
+}
